5d1d28df6badabbec093f5a1f4fddc7a322f8804
tu vieja xd <flor.p.vela@gmail.com>
2022-06-30 10:30:31 -0300

    xor

--- ejercicios_resueltos/utils/XOR.py
+++ ejercicios_resueltos/utils/XOR.py
@@ -17,20 +17,20 @@ class XOR:
 
         # weights and bias initialization
         # Layer 1
-        self.Z1_weights = np.random.uniform(size=(2, 2))
-        self.Z1_bias = np.random.uniform(size=(1, 2))
+        self.layer_1_weights = np.random.uniform(size=(2, 2))
+        self.layer_1_bias = np.random.uniform(size=(1, 2))
         # Layer 2 - output
-        self.Z2_weights = np.random.uniform(size=(2, 1))
-        self.Z2_bias = np.random.uniform(size=(1, 1))
+        self.layer_2_weights = np.random.uniform(size=(2, 1))
+        self.layer_2_bias = np.random.uniform(size=(1, 1))
 
     def forward_propagation(self, X):
-        Z1_activation = np.dot(X, self.Z1_weights)
-        Z1_activation += self.Z1_bias
-        self.Z1_output = sigmoid(Z1_activation)
+        layer_1_activation = np.dot(X, self.layer_1_weights)
+        layer_1_activation += self.layer_1_bias
+        self.layer_1_output = sigmoid(layer_1_activation)
 
-        Z2_activation = np.dot(self.Z1_output, self.Z2_weights)
-        Z2_activation += self.Z2_bias
-        prediction = sigmoid(Z2_activation)
+        layer_2_activation = np.dot(self.layer_1_output, self.layer_2_weights)
+        layer_2_activation += self.layer_2_bias
+        prediction = sigmoid(layer_2_activation)
 
         return prediction
 
@@ -38,14 +38,16 @@ class XOR:
         error = y - prediction
         d_prediction = error * sigmoid_deriv(prediction)
 
-        error_Z1 = d_prediction.dot(self.Z2_weights.T)
-        d_Z1 = error_Z1 * sigmoid_deriv(self.Z1_output)
+        error_layer_1 = d_prediction.dot(self.layer_2_weights.T)
+        d_layer_1 = error_layer_1 * sigmoid_deriv(self.layer_1_output)
 
         # Updating Weights and Biases
-        self.Z2_weights += self.Z1_output.T.dot(d_prediction) * self.lr
-        self.Z2_bias += np.sum(d_prediction, axis=0, keepdims=True) * self.lr
-        self.Z1_weights += X.T.dot(d_Z1) * self.lr
-        self.Z1_bias += np.sum(d_Z1, axis=0, keepdims=True) * self.lr
+        self.layer_2_weights += self.layer_1_output.T.dot(d_prediction) * self.lr
+        self.layer_2_bias += np.sum(d_prediction, axis=0, keepdims=True) * self.lr
+        self.layer_1_weights += X.T.dot(d_layer_1) * self.lr
+        self.layer_1_bias += np.sum(d_layer_1, axis=0, keepdims=True) * self.lr
+
+        # print(self.layer_2_bias.shape, self.layer_1_bias.shape) # (1, 1) (1, 2)
 
     def train(self, X, y):
         for _ in range(self.n_epochs):
