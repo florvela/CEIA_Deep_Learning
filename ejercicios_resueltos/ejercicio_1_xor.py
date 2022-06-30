ad706360b7259588e8f29ed96075de90940b55a6
tu vieja xd <flor.p.vela@gmail.com>
2022-06-30 17:15:38 -0300

    XOR with NNet

--- ejercicios_resueltos/ejercicio_1_xor.py
+++ ejercicios_resueltos/ejercicio_1_xor.py
@@ -1,19 +1,22 @@
 import numpy as np
-from utils.XOR import XOR
+import matplotlib.pyplot as plt
+from utils.XOR_v2 import XOR
 
 # XOR dataset
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([[0], [1], [1], [0]])
 
-xor_obj = XOR(n_epochs=10000, lr=0.1)
+xor_obj = XOR(n_epochs=5000, lr=0.1)
 xor_obj.train(X, y)
 
 ################ tests ################
-test = np.array([[1, 0]])
-assert xor_obj.predict(test) == 1
-test = np.array([[0, 0]])
-assert xor_obj.predict(test) == 0
-test = np.array([[0, 1]])
-assert xor_obj.predict(test) == 1
-test = np.array([[1, 1]])
-assert xor_obj.predict(test) == 0
+assert xor_obj.predict([1, 0]) == 1
+assert xor_obj.predict([0, 0]) == 0
+assert xor_obj.predict([0, 1]) == 1
+assert xor_obj.predict([1, 1]) == 0
+
+################ MSE ################
+plt.plot(xor_obj.MSE)
+plt.xlabel("Epochs")
+plt.ylabel("MSE")
+plt.show()
--- ejercicios_resueltos/utils/XOR.py
+++ ejercicios_resueltos/utils/XOR.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -14,50 +14,70 @@ class XOR:
     def __init__(self, n_epochs, lr):
         self.n_epochs = n_epochs
         self.lr = lr
+        self.MSE = list()
 
         # weights and bias initialization
         # Layer 1
-        self.layer_1_weights = np.random.uniform(size=(2, 2))
-        self.layer_1_bias = np.random.uniform(size=(1, 2))
+        self.layer_1_w1 = random.uniform(0, 1)
+        self.layer_1_w2 = random.uniform(0, 1)
+        self.layer_1_w3 = random.uniform(0, 1)
+        self.layer_1_w4 = random.uniform(0, 1)
+        self.layer_1_bias1 = random.uniform(0, 1)
+        self.layer_1_bias2 = random.uniform(0, 1)
+
         # Layer 2 - output
-        self.layer_2_weights = np.random.uniform(size=(2, 1))
-        self.layer_2_bias = np.random.uniform(size=(1, 1))
+        self.layer_2_w1 = random.uniform(0, 1)
+        self.layer_2_w2 = random.uniform(0, 1)
+        self.layer_2_bias = random.uniform(0, 1)
 
     def forward_propagation(self, X):
-        layer_1_activation = np.dot(X, self.layer_1_weights)
-        layer_1_activation += self.layer_1_bias
-        self.layer_1_output = sigmoid(layer_1_activation)
+        x1, x2 = X[0], X[1]
+        layer_1_z1 = self.layer_1_w1 * x1 + self.layer_1_w3 * x2 + self.layer_1_bias1
+        layer_1_z2 = self.layer_1_w2 * x1 + self.layer_1_w4 * x2 + self.layer_1_bias2
+        layer_1_z1_output = sigmoid(layer_1_z1)
+        layer_1_z2_output = sigmoid(layer_1_z2)
 
-        layer_2_activation = np.dot(self.layer_1_output, self.layer_2_weights)
-        layer_2_activation += self.layer_2_bias
-        prediction = sigmoid(layer_2_activation)
+        layer_2_z1 = self.layer_2_w1 * layer_1_z1_output + self.layer_2_w2 * layer_1_z2_output + self.layer_1_bias2
+        layer_2_z1_output = layer_2_z1
 
-        return prediction
+        prediction = layer_2_z1_output
+        return prediction, layer_1_z1_output, layer_1_z2_output
 
-    def back_propagation(self, X, y, prediction):
-        error = y - prediction
-        d_prediction = error * sigmoid_deriv(prediction)
+    def back_propagation(self, X, layer_1_z1_output, layer_1_z2_output, error):
+        x1, x2 = X[0], X[1]
 
-        error_layer_1 = d_prediction.dot(self.layer_2_weights.T)
-        d_layer_1 = error_layer_1 * sigmoid_deriv(self.layer_1_output)
+        d_prediction = - 2 * error
+        error_layer_1_z1 = d_prediction * self.layer_2_w1
+        error_layer_1_z2 = d_prediction * self.layer_2_w2
+        d_layer_1_z1 = error_layer_1_z1 * sigmoid_deriv(layer_1_z1_output)
+        d_layer_1_z2 = error_layer_1_z2 * sigmoid_deriv(layer_1_z2_output)
 
         # Updating Weights and Biases
-        self.layer_2_weights += self.layer_1_output.T.dot(d_prediction) * self.lr
-        self.layer_2_bias += np.sum(d_prediction, axis=0, keepdims=True) * self.lr
-        self.layer_1_weights += X.T.dot(d_layer_1) * self.lr
-        self.layer_1_bias += np.sum(d_layer_1, axis=0, keepdims=True) * self.lr
+        self.layer_2_w1 -= self.lr * d_prediction * layer_1_z1_output
+        self.layer_2_w2 -= self.lr * d_prediction * layer_1_z2_output
+        self.layer_2_bias -= d_prediction * self.lr
+        self.layer_1_w1 -= self.lr * (d_layer_1_z1 * x1)
+        self.layer_1_w3 -= self.lr * (d_layer_1_z1 * x2)
+        self.layer_1_w2 -= self.lr * (d_layer_1_z2 * x1)
+        self.layer_1_w4 -= self.lr * (d_layer_1_z2 * x2)
+        self.layer_1_bias1 -= self.lr * (d_layer_1_z1)
+        self.layer_1_bias2 -= self.lr * (d_layer_1_z2)
 
-        # print(self.layer_2_bias.shape, self.layer_1_bias.shape) # (1, 1) (1, 2)
 
     def train(self, X, y):
         for _ in range(self.n_epochs):
-            prediction = self.forward_propagation(X)
-            self.back_propagation(X, y, prediction)
+            iteration_error = 0
+            for i in range(len(X)):
+                prediction, layer_1_z1_output, layer_1_z2_output = self.forward_propagation(X[i])
+                error = y[i] - prediction
+                iteration_error += error ** 2
+                self.back_propagation(X[i], layer_1_z1_output, layer_1_z2_output, error)
+            self.MSE.append(iteration_error/4)
 
     def predict(self, input):
-        prediction = self.forward_propagation(input)
+        prediction, layer_1_z1_output, layer_1_z2_output = self.forward_propagation(input)
 
-        if prediction >= 0.5:
+        if prediction.squeeze() >= 0.5:
             return 1
         else:
             return 0
